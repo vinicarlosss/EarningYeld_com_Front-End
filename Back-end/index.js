49bec9429c230463b12552050d5c81bab126afa5
Carlos Vinícius <carlos.viniciusufrpe@gmail.com>
2022-01-12 06:54:04 -0300

    criada rota de get para empresas

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,8 +1,11 @@
-import express from "express";
+const express = require('express');
 
 const APP = express();
 
 APP.use(express.json());
+
+APP.use(require('./src/routes/routes.js'));
+
 
 APP.listen(5000, ()=>{
     console.log("Server on port: 5000")
--- Back-end/src/routes/routes.js
+++ Back-end/src/routes/routes.js
@@ -1,1 +1,45 @@
+const express = require('express');
+const mysql = require('mysql2');
+const router = express.Router();
+const cors = require('cors');
 
+router.use(cors());
+router.use(express.json());
+//String de conexão
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    port: 3306,
+    password: '1998ronca',
+    database: 'formula_magica'
+});
+
+//conexão com o banco
+
+connection.connect(function(error)
+    {
+        if(!!error){
+            console.log(error)
+        }else{
+            console.log('Conectado')
+        }
+    }
+)
+
+router.get('/listar-empresa', (req,res)=>{
+    let ticker = 'ENAT3';
+    connection.query(`SELECT * FROM Empresa WHERE ticker = '${ticker}';`,
+    (error, rows) =>{
+        if(!!error){
+            res.send(error);
+        }else{
+            res.send(rows);
+        }
+    })
+})
+
+//Métodos para integração
+
+
+module.exports = router;
