21e894eb69dd3a212c98e1ae711832624cc8f10a
Carlos Vinícius <carlos.viniciusufrpe@gmail.com>
2022-01-12 07:34:31 -0300

    adicionada rota de listar ranking

--- Back-end/src/routes/routes.js
+++ Back-end/src/routes/routes.js
@@ -3,6 +3,7 @@ const mysql = require('mysql2');
 const router = express.Router();
 const cors = require('cors');
 
+
 router.use(cors());
 router.use(express.json());
 //String de conexão
@@ -27,8 +28,10 @@ connection.connect(function(error)
     }
 )
 
+//rota para requisitar dados de uma empresa
 router.get('/listar-empresa', (req,res)=>{
-    let ticker = 'ENAT3';
+    let ticker = prompt('digite');
+    console.log(req)
     connection.query(`SELECT * FROM Empresa WHERE ticker = '${ticker}';`,
     (error, rows) =>{
         if(!!error){
@@ -37,7 +40,20 @@ router.get('/listar-empresa', (req,res)=>{
             res.send(rows);
         }
     })
-})
+});
+
+//rota para requisitar dados do ranking
+router.get('/listar-ranking', (req,res)=>{
+    connection.query('SELECT * FROM ranking ORDER BY earning_yeld DESC',
+    (error, rows) =>{
+        if(!!error){
+            res.send(error);
+        }else{
+            res.send(rows);
+        }
+    })
+    
+});
 
 //Métodos para integração
 
